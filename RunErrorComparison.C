0f8920e9cc0a4db86814aff5a68455a01f7c4a41
adaq <apar@adaq1.jlab.org>
2021-03-15 19:41:49 -0400

    added extra plots.

--- RunErrorComparison.C
+++ RunErrorComparison.C
@@ -8,8 +8,11 @@ void RunErrorComparison(Int_t Run){
     file1->cd();
 
     TTree* mul = (TTree*)gROOT->FindObject("mul");
+    TTree* mulc_lrb_burst = (TTree*)gROOT->FindObject("mulc_lrb_burst");
 
     mul->AddFriend("new = mul",file2);
+    mul->AddFriend("old_mulc_lrb_burst = mulc_lrb_burst",file1);
+    mul->AddFriend("new_mulc_lrb_burst = mulc_lrb_burst",file2);
 
     TCanvas *c1 = new TCanvas();
     mul->Draw("new.asym_usl:Entry$>>usl_main2D()","new.ErrorFlag==0","goff");
@@ -34,6 +37,74 @@ void RunErrorComparison(Int_t Run){
     usl_new->Draw();
     c2->SaveAs("./ComparisonOutputs/Plots/temp/usl.pdf");
 
+    TCanvas *c1a = new TCanvas();
+    mul->Draw("new.yield_usl:Entry$>>yusl_main2D()","new.ErrorFlag==0","goff");
+    TH2F *yusl_main2D = (TH2F*)gROOT->FindObject("yusl_main2D");
+    mul->Draw("new.yield_usl:Entry$>>yusl_new2D()","ErrorFlag!=0 && new.ErrorFlag==0","goff");
+    TH2F *yusl_new2D = (TH2F*)gROOT->FindObject("yusl_new2D");
+    yusl_new2D->SetMarkerStyle(7);
+    yusl_new2D->SetMarkerColor(2);
+    yusl_main2D->Draw();
+    yusl_new2D->Draw("same");
+    c1a->SaveAs("./ComparisonOutputs/Plots/temp/yusl2D.pdf");
+
+    TCanvas *c2a = new TCanvas();
+    c2a->Divide(2,1);
+    mul->Draw("new.yield_usl>>yusl_main()","new.ErrorFlag==0","goff");
+    TH2F *yusl_main = (TH2F*)gROOT->FindObject("yusl_main");
+    mul->Draw("new.yield_usl>>yusl_new()","ErrorFlag!=0 && new.ErrorFlag==0","goff");
+    TH2F *yusl_new = (TH2F*)gROOT->FindObject("yusl_new");
+    c2a->cd(1);
+    yusl_main->Draw();
+    c2a->cd(2);
+    yusl_new->Draw();
+    c2a->SaveAs("./ComparisonOutputs/Plots/temp/yusl.pdf");
+
+    TCanvas *c1b = new TCanvas();
+    mul->Draw("new.asym_usr:Entry$>>usr_main2D()","new.ErrorFlag==0","goff");
+    TH2F *usr_main2D = (TH2F*)gROOT->FindObject("usr_main2D");
+    mul->Draw("new.asym_usr:Entry$>>usr_new2D()","ErrorFlag!=0 && new.ErrorFlag==0","goff");
+    TH2F *usr_new2D = (TH2F*)gROOT->FindObject("usr_new2D");
+    usr_new2D->SetMarkerStyle(7);
+    usr_new2D->SetMarkerColor(2);
+    usr_main2D->Draw();
+    usr_new2D->Draw("same");
+    c1b->SaveAs("./ComparisonOutputs/Plots/temp/usr2D.pdf");
+
+    TCanvas *c2b = new TCanvas();
+    c2b->Divide(2,1);
+    mul->Draw("new.asym_usr>>usr_main()","new.ErrorFlag==0","goff");
+    TH2F *usr_main = (TH2F*)gROOT->FindObject("usr_main");
+    mul->Draw("new.asym_usr>>usr_new()","ErrorFlag!=0 && new.ErrorFlag==0","goff");
+    TH2F *usr_new = (TH2F*)gROOT->FindObject("usr_new");
+    c2b->cd(1);
+    usr_main->Draw();
+    c2b->cd(2);
+    usr_new->Draw();
+    c2b->SaveAs("./ComparisonOutputs/Plots/temp/usr.pdf");
+
+    TCanvas *c1ba = new TCanvas();
+    mul->Draw("new.yield_usr:Entry$>>yusr_main2D()","new.ErrorFlag==0","goff");
+    TH2F *yusr_main2D = (TH2F*)gROOT->FindObject("yusr_main2D");
+    mul->Draw("new.yield_usr:Entry$>>yusr_new2D()","ErrorFlag!=0 && new.ErrorFlag==0","goff");
+    TH2F *yusr_new2D = (TH2F*)gROOT->FindObject("yusr_new2D");
+    yusr_new2D->SetMarkerStyle(7);
+    yusr_new2D->SetMarkerColor(2);
+    yusr_main2D->Draw();
+    yusr_new2D->Draw("same");
+    c1ba->SaveAs("./ComparisonOutputs/Plots/temp/yusr2D.pdf");
+
+    TCanvas *c2ba = new TCanvas();
+    c2ba->Divide(2,1);
+    mul->Draw("new.yield_usr>>yusr_main()","new.ErrorFlag==0","goff");
+    TH2F *yusr_main = (TH2F*)gROOT->FindObject("yusr_main");
+    mul->Draw("new.yield_usr>>yusr_new()","ErrorFlag!=0 && new.ErrorFlag==0","goff");
+    TH2F *yusr_new = (TH2F*)gROOT->FindObject("yusr_new");
+    c2ba->cd(1);
+    yusr_main->Draw();
+    c2ba->cd(2);
+    yusr_new->Draw();
+    c2ba->SaveAs("./ComparisonOutputs/Plots/temp/yusr.pdf");
 
     TCanvas *c3 = new TCanvas();
     mul->Draw("new.asym_bcm_an_us:Entry$>>bcm_main2D()","new.ErrorFlag==0","goff");
@@ -58,6 +129,28 @@ void RunErrorComparison(Int_t Run){
     bcm_new->Draw();
     c4->SaveAs("./ComparisonOutputs/Plots/temp/bcm.pdf");
 
+    TCanvas *c3a = new TCanvas();
+    mul->Draw("new.yield_bcm_an_us:Entry$>>ybcm_main2D()","new.ErrorFlag==0","goff");
+    TH2F *ybcm_main2D = (TH2F*)gROOT->FindObject("ybcm_main2D");
+    mul->Draw("new.yield_bcm_an_us:Entry$>>ybcm_new2D()","ErrorFlag!=0 && new.ErrorFlag==0","goff");
+    TH2F *ybcm_new2D = (TH2F*)gROOT->FindObject("ybcm_new2D");
+    ybcm_new2D->SetMarkerStyle(7);
+    ybcm_new2D->SetMarkerColor(2);
+    ybcm_main2D->Draw();
+    ybcm_new2D->Draw("same");
+    c3a->SaveAs("./ComparisonOutputs/Plots/temp/ybcm2D.pdf");
+
+    TCanvas *c4a = new TCanvas();
+    c4a->Divide(2,1);
+    mul->Draw("new.yield_bcm_an_us>>ybcm_main()","new.ErrorFlag==0","goff");
+    TH2F *ybcm_main = (TH2F*)gROOT->FindObject("ybcm_main");
+    mul->Draw("new.yield_bcm_an_us>>ybcm_new()","ErrorFlag!=0 && new.ErrorFlag==0","goff");
+    TH2F *ybcm_new = (TH2F*)gROOT->FindObject("ybcm_new");
+    c4a->cd(1);
+    ybcm_main->Draw();
+    c4a->cd(2);
+    ybcm_new->Draw();
+    c4a->SaveAs("./ComparisonOutputs/Plots/temp/ybcm.pdf");
 
     TCanvas *c5 = new TCanvas();
     mul->Draw("new.yield_bpm4aX:Entry$>>bpm4aX_main2D()","new.ErrorFlag==0","goff");
@@ -129,6 +222,52 @@ void RunErrorComparison(Int_t Run){
     c10->cd(2);
     bpm12X_new->Draw();
     c10->SaveAs("./ComparisonOutputs/Plots/temp/bpm12X.pdf");
+
+    TCanvas *c11 = new TCanvas();
+    mul->Draw("new_mulc_lrb_burst.cor_asym_us_avg:Entry$>>cor_asym_us_avg_main2D()","new.ErrorFlag==0","goff");
+    TH2F *cor_asym_us_avg_main2D = (TH2F*)gROOT->FindObject("cor_asym_us_avg_main2D");
+    mul->Draw("new_mulc_lrb_burst.cor_asym_us_avg:Entry$>>cor_asym_us_avg_new2D()","ErrorFlag!=0 && new.ErrorFlag==0","goff");
+    TH2F *cor_asym_us_avg_new2D = (TH2F*)gROOT->FindObject("cor_asym_us_avg_new2D");
+    cor_asym_us_avg_new2D->SetMarkerStyle(7);
+    cor_asym_us_avg_new2D->SetMarkerColor(2);
+    cor_asym_us_avg_main2D->Draw();
+    cor_asym_us_avg_new2D->Draw("same");
+    c11->SaveAs("./ComparisonOutputs/Plots/temp/cor_asym_us_avg2D.pdf");
+
+    TCanvas *c12 = new TCanvas();
+    c12->Divide(2,1);
+    mul->Draw("new_mulc_lrb_burst.cor_asym_us_avg>>cor_asym_us_avg_main()","new.ErrorFlag==0","goff");
+    TH2F *cor_asym_us_avg_main = (TH2F*)gROOT->FindObject("cor_asym_us_avg_main");
+    mul->Draw("new_mulc_lrb_burst.cor_asym_us_avg>>cor_asym_us_avg_new()","ErrorFlag!=0 && new.ErrorFlag==0","goff");
+    TH2F *cor_asym_us_avg_new = (TH2F*)gROOT->FindObject("cor_asym_us_avg_new");
+    c12->cd(1);
+    cor_asym_us_avg_main->Draw();
+    c12->cd(2);
+    cor_asym_us_avg_new->Draw();
+    c12->SaveAs("./ComparisonOutputs/Plots/temp/cor_asym_us_avg.pdf");
+
+    TCanvas *c13 = new TCanvas();
+    mul->Draw("new_mulc_lrb_burst.cor_asym_us_dd:Entry$>>cor_asym_us_dd_main2D()","new.ErrorFlag==0","goff");
+    TH2F *cor_asym_us_dd_main2D = (TH2F*)gROOT->FindObject("cor_asym_us_dd_main2D");
+    mul->Draw("new_mulc_lrb_burst.cor_asym_us_dd:Entry$>>cor_asym_us_dd_new2D()","ErrorFlag!=0 && new.ErrorFlag==0","goff");
+    TH2F *cor_asym_us_dd_new2D = (TH2F*)gROOT->FindObject("cor_asym_us_dd_new2D");
+    cor_asym_us_dd_new2D->SetMarkerStyle(7);
+    cor_asym_us_dd_new2D->SetMarkerColor(2);
+    cor_asym_us_dd_main2D->Draw();
+    cor_asym_us_dd_new2D->Draw("same");
+    c13->SaveAs("./ComparisonOutputs/Plots/temp/cor_asym_us_dd2D.pdf");
+
+    TCanvas *c14 = new TCanvas();
+    c14->Divide(2,1);
+    mul->Draw("new_mulc_lrb_burst.cor_asym_us_dd>>cor_asym_us_dd_main()","new.ErrorFlag==0","goff");
+    TH2F *cor_asym_us_dd_main = (TH2F*)gROOT->FindObject("cor_asym_us_dd_main");
+    mul->Draw("new_mulc_lrb_burst.cor_asym_us_dd>>cor_asym_us_dd_new()","ErrorFlag!=0 && new.ErrorFlag==0","goff");
+    TH2F *cor_asym_us_dd_new = (TH2F*)gROOT->FindObject("cor_asym_us_dd_new");
+    c14->cd(1);
+    cor_asym_us_dd_main->Draw();
+    c14->cd(2);
+    cor_asym_us_dd_new->Draw();
+    c14->SaveAs("./ComparisonOutputs/Plots/temp/cor_asym_us_dd.pdf");
 
     gSystem->Exec(Form("pdfunite ./ComparisonOutputs/Plots/temp/* ./ComparisonOutputs/Plots/summary%d.pdf", Run));
     
